efad520eed83c606c4c8247e46b3af59a598e440
Mike Fluff <mf@mikefluff.com>
2018-03-07 00:16:51 +0700

    PIN request added, session cleanup added

--- index.js
+++ index.js
@@ -8,6 +8,36 @@ var bippath = require('bip32-path')
 
 var debug = false;
 
+/**
+ * @param {string} type
+ * @param {Function<Error, string>} callback
+ */
+function pinCallback(type, callback) {
+    console.log('Please enter PIN. The positions:');
+    console.log('7 8 9');
+    console.log('4 5 6');
+    console.log('1 2 3');
+
+    // note - disconnecting the device should trigger process.stdin.pause too, but that
+    // would complicate the code
+
+    // we would need to pass device in the function and call device.on('disconnect', ...
+
+    process.stdin.resume();
+    process.stdin.on('data', function (buffer) {
+        var text = buffer.toString().replace(/\n$/, "");
+        process.stdin.pause();
+        callback(null, text);
+    });
+}
+
+// an example function, that asks user for acquiring and
+// calls callback if use agrees
+// (in here, we will call agree always, since it's just an example)
+function askUserForceAcquire(callback) {
+    return setTimeout(callback, 1000);
+}
+
 function normalize(hex) {
 	if (hex == null) {
 		return null;
@@ -28,6 +58,7 @@ function buffer(hex) {
 		return new Buffer(normalize(hex), 'hex');
 	}
 }
+var wait = ms => new Promise((r, j)=>setTimeout(r, ms))
 
 var trezorInstance;
 
@@ -42,11 +73,7 @@ class Trezor {
 			var dev = device;
 	        console.log("Connected device " + device.features.label);
 	        self.devices.push(device);
-
-		    device.on('pin', (type, callback) => {
-		        console.error("Entering pin is not supported. Unlock TREZOR in other app!");
-		        callback(new Error());
-		    });
+ device.on('pin', pinCallback);
 
 	        // For convenience, device emits 'disconnect' event on disconnection.
 	        device.on('disconnect', function () {
@@ -58,20 +85,29 @@ class Trezor {
 	        if (device.isBootloader()) {
 	            throw new Error('Device is in bootloader mode, re-connected it');
 	        }
+		 // low level API
 	    });
+		// On connecting unacquired device
+this.list.on('connectUnacquired', function (device) {
+    askUserForceAcquire(function() {
+        device.steal().then(function() {
+            console.log("steal done. now wait for another connect");
+        });
+    });
+});
+
 	}
+        
+	async inTrezorSession(cb) {
 
-	inTrezorSession(cb) {
-        if (this.devices.length == 0) {
-            return Promise.reject(new Error("no device connected"));
-        } else {
-            return this.devices[0].waitForSessionAndRun(cb);
-        }
-    }
+            //return Promise.reject(new Error("no device connected"));
+        return this.devices[0].waitForSessionAndRun(cb);
+	 }
 
-	getAccounts(cb) {
+	async getAccounts(cb) {
+		await wait(3000)
 		var self = this;
-	    this.inTrezorSession(
+	    await this.inTrezorSession(
 	        session => session.ethereumGetAddress(self.path, false)
 	    )
 	    .then(resp => "0x" + resp.message.address)
